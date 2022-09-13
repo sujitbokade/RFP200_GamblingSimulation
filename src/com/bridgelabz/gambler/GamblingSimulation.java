2b97cb44d1a30dbf7de7fe5c9d672c60064b1c5d
sujit <sujitbokade2014@gmail.com>
2022-09-13 22:10:33 +0530

    [Sujit]Check Win Or Loss

--- src/com/bridgelabz/gambler/GamblingSimulation.java
+++ src/com/bridgelabz/gambler/GamblingSimulation.java
@@ -3,7 +3,18 @@ package com.bridgelabz.gambler;
 public class GamblingSimulation {
     static int Daily_Stake = 100;
     static int Bet_Per_Game = 1;
+    static int win = 1;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Simulation");
+        int random = (int) (Math.random() * 10) % 2;
+        if (random == win) {
+            System.out.println("You Won");
+            Daily_Stake++;
+        } else {
+            System.out.println("You Loss");
+            Daily_Stake--;
+        }
+        System.out.println("Dily Stake " + Daily_Stake);
     }
 }
