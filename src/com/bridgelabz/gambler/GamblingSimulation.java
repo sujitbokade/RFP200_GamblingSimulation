ceff0d3ea93e247448ad21a8c91bb70d9de8eb96
sujit <sujitbokade2014@gmail.com>
2022-09-13 22:01:32 +0530

    [Sujit]Start Stake

--- src/com/bridgelabz/gambler/GamblingSimulation.java
+++ src/com/bridgelabz/gambler/GamblingSimulation.java
@@ -1,6 +1,8 @@
 package com.bridgelabz.gambler;
 
 public class GamblingSimulation {
+    static int Daily_Stake = 100;
+    static int Bet_Per_Game = 1;
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Simulation");
     }
