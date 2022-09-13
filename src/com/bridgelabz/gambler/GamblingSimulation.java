7925264f0f0cd90f01d3d452295372824d40dc92
sujit <sujitbokade2014@gmail.com>
2022-09-13 20:36:48 +0530

    [Sujit]Welcome Message

---
+++ src/com/bridgelabz/gambler/GamblingSimulation.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.gambler;
+
+public class GamblingSimulation {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Gambling Simulation");
+    }
+}
